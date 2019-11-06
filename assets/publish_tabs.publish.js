e8c8b8a9284790e2e20e9ce5fa58e02519606d1d
Marc-André Charpentier <marc-andre@deuxhuithuit.com>
2019-11-06 17:24:57 -0500

    design refactor in progress; add real anchor links in tabs; include activation by intersectionObserver; In progress

--- assets/publish_tabs.publish.js
+++ assets/publish_tabs.publish.js
@@ -13,6 +13,29 @@ Symphony.Language.add({
 
 	'use strict';
 
+
+	var options = {
+	  root: $('.primary').this,
+	  rootMargin: '0px',
+	  threshold: 1.0
+	};
+
+	function callback(entries, observer) {
+		$(entries).each(function() {
+			if (this.intersectionRatio >= 1) {
+				// console.log("Fully visible!");
+				var selector = this.target.getAttribute('id');
+				console.log(selector);
+				$('.publishtabs').find('[href="#' + selector + '"]').click();
+			} else {
+			// console.log("Not fully visible!");
+			}
+			
+		});
+	}
+
+	var observer = new IntersectionObserver(callback, options);
+
 	var localStorage = window.localStorage || {};
 
 	window.PublishTabs = {
@@ -23,7 +46,7 @@ Symphony.Language.add({
 
 		init: function() {
 			var self = this;
-			var context = $('#contents-actions');
+			var context = $('#context');
 
 			// thy shalt not pass if no Publish Tab fields used
 			var tab_fields = $('.field-publish_tabs');
@@ -41,7 +64,7 @@ Symphony.Language.add({
 			this.new_entry = (env.page === 'new');
 
 			var has_invalid_tabs = false;
-			this.tab_controls = $('<ul class="publishtabs"></ul>');
+			this.tab_controls = $('<ul class="tabs publishtabs"></ul>');
 
 			var publish_tabs = Symphony.Context.get('publish-tabs');
 
@@ -59,20 +82,21 @@ Symphony.Language.add({
 				var $main_fields = $(main_fields);
 				var $sidebar_fields = $(sidebar_fields);
 
-				$main_fields.wrapAll('<div class="tab-group tab-group-' + publish_tabs[i]['tab_id'] + '"></div>');
-				$sidebar_fields.wrapAll('<div class="tab-group tab-group-' + publish_tabs[i]['tab_id'] + '"></div>');
+				$main_fields.wrapAll('<div id="tab-group-' + publish_tabs[i]['tab_id'] + '" class="tab-group tab-group-' + publish_tabs[i]['tab_id'] + '"></div>');
+				$sidebar_fields.wrapAll('<div id="tab-group-' + publish_tabs[i]['tab_id'] + '" class="tab-group tab-group-' + publish_tabs[i]['tab_id'] + '"></div>');
 
 				var tab_field = $('#field-' + publish_tabs[i]['tab_id']).remove();
 				var tab_text = (tab_field.text() != '') ? tab_field.text() : Symphony.Language.get('Untitled Tab');
-				var tab_button = $('<li class="tab-'+publish_tabs[i]['tab_id']+'" data-id="'+publish_tabs[i]['tab_id']+'">' + tab_text + '</li>');
-
-				this.tab_controls.append(tab_button);
+				var tab_item = $('<li></li>');
+				var tab_button = $('<a class="tab-'+publish_tabs[i]['tab_id']+'" data-id="'+publish_tabs[i]['tab_id']+'" href="#tab-group-' + publish_tabs[i]['tab_id'] + '">' + tab_text + '</a>');
+				tab_item.append(tab_button);
+				this.tab_controls.append(tab_item);
 
 				// add click event to tab
 				tab_button.on('click', function (e) {
 					var t = $(this);
 					var id = t.attr('data-id');
-					if (t.hasClass('selected')) return;
+					if (t.hasClass('active')) return;
 					if (!!id) {
 						self.showTab(id);
 						// if it's a real user click
@@ -80,6 +104,8 @@ Symphony.Language.add({
 							self.saveLocalTab('publish-tab', id);
 						}
 					}
+
+					window.location.hash = '#' + t.attr('href').split('#')[1];
 				});
 
 				// find invalid fields
@@ -87,43 +113,53 @@ Symphony.Language.add({
 					has_invalid_tabs = true;
 					tab_button.addClass('invalid').append('<span class="icon">!</span>');
 				}
+
 			}
 
 			// prepend tags controls
 			context.prepend(this.tab_controls);
 
-			// selected the right tab
+			// activate the right tab
 			if (has_invalid_tabs) {
-				this.tab_controls.find('li.invalid:first').click();
+				this.tab_controls.find('.invalid').first().click();
 			} else {
 				var initial_tab = self.getURLParameter('publish-tab');
 				var local_tab = self.getLocalTab('publish-tab');
 
-				var selector = !!initial_tab ? '.' + initial_tab : (!!local_tab ? local_tab : 'li:first');
+				var selector = !!initial_tab ? '.' + initial_tab : (!!local_tab ? local_tab : 'li:first a');
 				this.tab_controls.find(selector).click();
 			}
+
+			// Start IntersectionObserver on groups
+			var tab_groups = $('.tab-group');
+			tab_groups.each(function(){
+				observer.observe(this);
+			});
 		},
 
 		showTab: function(tab) {
 			var w = $('#contents').width();
 
 			// de-select current tab and select the new tab
-			this.tab_controls.find('li.selected').removeClass('selected');
-			this.tab_controls.find('li.tab-' + tab).addClass('selected');
+			this.tab_controls.find('li.active').removeClass('active');
+			this.tab_controls.find('a.tab-' + tab).parent('li').addClass('active');
 
 			// hide current tab group and select new group
-			$('.tab-group-selected').removeClass('tab-group-selected');
-			$('.tab-group-' + tab).addClass('tab-group-selected');
+			$('.tab-group-active').removeClass('tab-group-active');
+			$('.tab-group-' + tab).addClass('tab-group-active');
 
 			var invalid_field = $('.tab-group-' + tab + ' .invalid');
 			// focus first invalid element
 			if (invalid_field.length) {
 				invalid_field.eq(0).find('*[name*="fields["]').focus();
 			}
+
 			// focus first field in tab when creating a new entry
 			else if (this.new_entry) {
 				$('.tab-group-' + tab + ' .field:first *[name*="fields["]').focus();
 			}
+			
+			return true;
 		},
 
 		getURLParameter: function(name) {
